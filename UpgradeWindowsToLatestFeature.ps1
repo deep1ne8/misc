72fe59daf83239c340c1c081ebb390aedc44fe43
deep1ne8 <31950845+deep1ne8@users.noreply.github.com>
2025-01-16 20:08:03 -0500

    Update UpgradeWindowsToLatestFeature.ps1

--- UpgradeWindowsToLatestFeature.ps1
+++ UpgradeWindowsToLatestFeature.ps1
@@ -56,7 +56,7 @@ if (!(Test-Path "C:\WindowsSetup")) {
 # Define file paths and URI
 $DownloadPath = "C:\WindowsSetup\Win_${Version}_${Release}_English_x64v1.iso"
 $FidoPath = "C:\WindowsSetup\Fido.ps1"
-$DownloadFido = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/deep1ne8/misc/refs/heads/main/Fido.ps1" -OutFile "C:\WindowsSetup\Fido.ps1" -Verbose -UseBasicParsing
+$DownloadFido = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/deep1ne8/misc/refs/heads/main/Fido.ps1" -OutFile "C:\WindowsSetup\Fido.ps1" -Verbose
 if (!(Test-Path $FidoPath)) {
     Write-Host "Fido.ps1 is not found in the current directory." -ForegroundColor Red
     Write-Host "Downloading Fido..."
@@ -66,8 +66,8 @@ if (!(Test-Path $FidoPath)) {
 # Check if the ISO file already exists
 if (!(Test-Path $DownloadPath)) {
     Log-Verbose "ISO not found at $DownloadPath. Starting download..."
-    $URI = & $FidoPath -Win $Version -Rel $Release -Arch x64 -Ed Pro -Lang English -GetUrl -Headers @{
-    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    Invoke-Expression -Command "& $FidoPath -Win $Version -Rel $Release -Arch x64 -Ed Pro -Lang English -GetUrl -Headers @{
+    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}"
 
     if (-not $URI) {
         Log-Verbose "Failed to retrieve download URL for Windows $Version $Release."
