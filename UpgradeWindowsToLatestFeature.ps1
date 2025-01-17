4cde0604f1b818ce09fc12e963a363195fdc80b1
deep1ne8 <31950845+deep1ne8@users.noreply.github.com>
2025-01-16 20:39:46 -0500

    Update UpgradeWindowsToLatestFeature.ps1

--- UpgradeWindowsToLatestFeature.ps1
+++ UpgradeWindowsToLatestFeature.ps1
@@ -66,8 +66,8 @@ if (!(Test-Path $FidoPath)) {
 # Check if the ISO file already exists
 if (!(Test-Path $DownloadPath)) {
     Log-Verbose "ISO not found at $DownloadPath. Starting download..."
-    $URI = .\C:\WindowsSetup\Fido.ps1 -Win $Version -Rel $Release -Arch x64 -Ed Pro -Lang English -GetUrl -Headers @{
-    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    $URI = (Invoke-Expression -Command "& '\C:\WindowsSetup\Fido.ps1' -Win $Version -Rel $Release -Arch x64 -Ed Pro -Lang English -GetUrl -Headers @{
+    'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}")
 
     if (-not $URI) {
         Log-Verbose "Failed to retrieve download URL for Windows $Version $Release."
