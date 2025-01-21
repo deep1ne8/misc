c5cb39162445cb11a8db13dd4e9309a491fcc300
deep1ne8 <31950845+deep1ne8@users.noreply.github.com>
2025-01-21 17:24:27 -0500

    Create MacVendorSearch.ps1

---
+++ MacVendorSearch.ps1
@@ -0,0 +1,15 @@
+# Import the CSV file containing MAC addresses and vendor information
+$Records = Import-CSV "Oui.csv"
+
+# Define the target PC MAC address to search for
+$PCMAC = '00-01-C8'
+
+# Iterate through each record in the CSV
+ForEach ($Record in $Records) {
+    # Check if the MAC address matches the target PC MAC address
+    if ($Record.MAC -eq $PCMAC) {
+        # Retrieve and output the vendor associated with the MAC address
+        $Vendor = $Record.Vendor
+        Write-Host $Vendor
+    }
+}
