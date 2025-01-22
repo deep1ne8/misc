b2df0888f85a62933934a1e34472b3a095add89a
deep1ne8 <31950845+deep1ne8@users.noreply.github.com>
2025-01-22 11:11:01 -0500

    Update EnableFilesOnDemand.ps1

--- EnableFilesOnDemand.ps1
+++ EnableFilesOnDemand.ps1
@@ -14,22 +14,32 @@
 
    # Output the result
    	if ($LoggedInUser) {
-    Write-Host "Logged-in User(s): $LoggedInUser"
+    Write-Host "Logged-in User(s): $LoggedInUser" -ForeGroundColor Green
 	} else {
-    Write-Host "No logged-in users found."
+    Write-Host "No logged-in users found." -ForeGroundColor Red
     }
 
-$CheckFilesAttrib = Get-childitem -Path "*.*" -Force | Format-Table Attributes
+$CheckFilesAttrib = Get-childitem -Path '*.*' -Force | Format-Table Attributes
 if ($CheckFilesAttrib -eq "5248544"){
 	Write-Host "All the files attributes are already set to online only"
  	exit 1
   }else {
-Set-Location -Path "$env:SystemDrive\Users\$LoggedInUser\Onedrive - *\" 
-Get-childitem -Path "*.*" -Force -File -Recurse -Verbose -ErrorAction SilentlyContinue |
+
+Start-Sleep -Seconds 3
+# Resolve OneDrive path dynamically
+$OneDrivePath = (Get-ChildItem -Path "$env:SystemDrive\Users\$LoggedInUser" -Directory | Where-Object { $_.Name -like "OneDrive - *" }).FullName
+if (-not $OneDrivePath) {
+    throw "OneDrive folder not found for user $LoggedInUser."
+}
+Set-Location -Path $OneDrivePath
+Start-Sleep -Seconds 3
+Get-childitem -Path '*.*' -Force -File -Recurse -Verbose -ErrorAction SilentlyContinue |
 Where-Object {$_.Attributes -match 'ReparsePoint' -or $_.Attributes -eq '525344' } |
 ForEach-Object {
     attrib.exe $_.fullname +U /s
 }
+Start-Sleep -Seconds 5
+Write-Host "`n"
 Write-Host "Below is the guide for the file state, according to it's attribute" -ForeGroundColor Green
 Write-Host "`n==================================================================="
 Write-Host "File State 	        Attribute" -ForeGroundColor White
@@ -38,8 +48,9 @@ Write-Host "Cloud-Only 	        5248544" -ForeGroundColor Green
 Write-Host "Always available 	525344" -ForeGroundColor Green
 Write-Host "Locally Available 	ReparsePoint" -ForeGroundColor Green
 Write-Host "`n==================================================================="
-
-Write-Host "Verifying the current file state"
+Start-Sleep -Seconds 5
+Write-Jost "`n"
+Write-Host "Verifying the current file state" -ForeGroundColor Green
 Write-Host ""
 Write-Output $CheckFilesAttrib
 }
