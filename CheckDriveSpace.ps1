c4344706eddf7d5bbadd4774b48d5538985bbe7a
Earl Daniels <deepster61@gmail.com>
2025-02-08 09:33:53 -0500

    update

--- CheckDriveSpace.ps1
+++ CheckDriveSpace.ps1
@@ -1,14 +1,18 @@
 # Check drive space
 $drive = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='C:'"
-$freeSpace = [math]::Round($drive.FreeSpace / 1GB, 2)
-$percentageFree = [math]::Round(($freeSpace / $drive.Size) * 100, 2)
+if ($drive) {
+    $freeSpace = [math]::Round($drive.FreeSpace / 1GB, 2)
+    $percentageFree = [math]::Round(($freeSpace / $drive.Size) * 100, 2)
 
-Write-Host "`nChecking drive space..."
-Write-Host "Drive: $($drive.DeviceID)"
-Write-Host "Free space: $freeSpace GB"
-Write-Host "Percentage free: $percentageFree%" -ForegroundColor Green
+    Write-Host "`nChecking drive space..."
+    Write-Host "Drive: $($drive.DeviceID)"
+    Write-Host "Free space: $freeSpace GB"
+    Write-Host "Percentage free: $percentageFree%" -ForegroundColor Green
 
-if ($percentageFree -lt 20) {
-    Write-Host "Low disk space! Please free up some space." -ForegroundColor Red
-    Start-Sleep -Seconds 5
+    if ($percentageFree -lt 20) {
+        Write-Host "Low disk space! Please free up some space." -ForegroundColor Red
+        Start-Sleep -Seconds 5
+    }
+} else {
+    Write-Host "Failed to retrieve drive information." -ForegroundColor Red
 }
