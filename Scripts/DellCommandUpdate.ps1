2e7dcbbd6af284e4662e7991ded45100755ff1f7
deep1ne <31950845+deep1ne8@users.noreply.github.com>
2025-08-30 07:20:34 -0400

    Update DellCommandUpdate.ps1
    Signed-off-by: deep1ne <31950845+deep1ne8@users.noreply.github.com>

--- Scripts/DellCommandUpdate.ps1
+++ Scripts/DellCommandUpdate.ps1
@@ -11,9 +11,6 @@
     - Handles reboot requirements automatically
 
 .NOTES
-    Author: Earl Daniels
-    Date: 2025-08-11
-    Version: 2.1
     Requires: PowerShell 5.1+, Administrator privileges
     Tested: Windows 10/11, Dell systems
 #>
