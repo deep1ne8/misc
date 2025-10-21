0df225723bde1de7b5e3840f19ef0f8998a4e143
deep1ne <31950845+deep1ne8@users.noreply.github.com>
2025-10-21 11:06:32 -0400

    Update script list in AutoByte.ps1
    Removed Intune GSA Diagnostics script and added Office and Windows Compatibility Check script.
    
    Signed-off-by: deep1ne <31950845+deep1ne8@users.noreply.github.com>

--- AutoByte.ps1
+++ AutoByte.ps1
@@ -20,7 +20,7 @@ Write-Host "✔ Registry updated to permanently enable strong crypto/TLS 1.2." -
 $GitHubScripts = @(
 
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/NetworkScan.ps1"; Description = "Network Scan" },
-    @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/IntuneGSADiag.ps1"; Description = "Intune GSA Diagnotics" },
+    @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/OfficeWindowsCompat.ps1"; Description = "Office and Windows Compatibility Check" },
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/Get-PrinterSupplies.ps1"; Description = "Get Printer Supplies" },
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/refs/heads/main/Scripts/WindowsOnlineRepair.ps1"; Description = "WindowsOnlineRepair" },
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/GetWindowsEvents.ps1"; Description = "Get Windows Events" },
