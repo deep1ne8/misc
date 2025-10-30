c5d498bc1220951bb287dbf5627892a3698764de
deep1ne <31950845+deep1ne8@users.noreply.github.com>
2025-10-30 16:09:13 -0400

    Update ScanUsers.ps1
    Signed-off-by: deep1ne <31950845+deep1ne8@users.noreply.github.com>

--- Scripts/ScanUsers.ps1
+++ Scripts/ScanUsers.ps1
@@ -1,11 +1,17 @@
-Get-ChildItem 'Registry::HKEY_USERS' | ForEach-Object {
-    $sid = $_.PSChildName
-    $key = "Registry::HKEY_USERS\$sid\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
-    if (Test-Path $key) {
-        [PSCustomObject]@{
-            SID        = $sid
-            Desktop    = (Get-ItemProperty -Path $key -Name 'Desktop' -ErrorAction SilentlyContinue).Desktop
-            Documents  = (Get-ItemProperty -Path $key -Name 'Personal' -ErrorAction SilentlyContinue).Personal
+Get-ChildItem "C:\Users" -Directory | ForEach-Object {
+    $userPath = "$($_.FullName)\NTUSER.DAT"
+    if (Test-Path $userPath) {
+        $sid = $_.Name
+        reg load "HKU\TempHive_$sid" $userPath | Out-Null
+        $key = "Registry::HKEY_USERS\TempHive_$sid\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+        if (Test-Path $key) {
+            $props = Get-ItemProperty -Path $key -ErrorAction SilentlyContinue
+            [PSCustomObject]@{
+                UserName  = $_.Name
+                Desktop   = $props.Desktop
+                Documents = $props.Personal
+            }
         }
+        reg unload "HKU\TempHive_$sid" | Out-Null
     }
 } | Where-Object { $_.Desktop -or $_.Documents } | Format-Table -AutoSize
