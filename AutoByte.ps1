8f6da38691a5d0dec3b0da6679c7861a87a501ca
deep1ne <31950845+deep1ne8@users.noreply.github.com>
2026-07-23 06:31:15 -0400

    Update script list in AutoByte.ps1
    Replaced GSuite_WorkSpace_Diag.ps1 with agent-installer.ps1 in the script list.
    
    Signed-off-by: deep1ne <31950845+deep1ne8@users.noreply.github.com>

--- AutoByte.ps1
+++ AutoByte.ps1
@@ -20,7 +20,7 @@ Write-Host "✔ Registry updated to permanently enable strong crypto/TLS 1.2." -
 $GitHubScripts = @(
 
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/DiskCleaner.ps1"; Description = "Windows C Drive Cleaner Tool" },
-    @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/GSuite_WorkSpace_Diag.ps1"; Description = "GWS HealthCheck" },
+    @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/agent-installer.ps1"; Description = "Empire Architectural Metal and Glass LG Agent Installer" },
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/Get-PrinterSupplies.ps1"; Description = "Get Printer Supplies" },
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/refs/heads/main/Scripts/WindowsOnlineRepair.ps1"; Description = "WindowsOnlineRepair" },
     @{ ScriptUrl = "https://raw.githubusercontent.com/deep1ne8/misc/main/Scripts/GetWindowsEvents.ps1"; Description = "Get Windows Events" },
